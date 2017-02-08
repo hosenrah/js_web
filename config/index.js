3a9ed146d7e4408cd80088e2e0aa1bfd7e526120
Andreas Rau <andreas.rau@de.ibm.com>
2017-02-08 13:23:01 +0100

    :lipstick: fixed static path for webpack build

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'js_web/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
