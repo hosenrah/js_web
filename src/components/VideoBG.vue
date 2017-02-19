3c0c1d347ef4fd6137931894e59529e78ea7b49d
Andreas Rau <andreas.rau93@gmail.com>
2017-02-19 23:29:02 +0100

    :sparkles: background video can now be selected by double #

--- src/components/VideoBG.vue
+++ src/components/VideoBG.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="videoWrapper">
-    <iframe id="videoBG" class="videBG" frameborder="0" src="https://player.vimeo.com/video/194162945?background=1"></iframe>
+    <iframe v-for="video in vimeoVideos" id="videoBG" class="videBG" frameborder="0" v-bind:src="'https://player.vimeo.com/video/' + video.uri.slice(8) + '?background=1'"></iframe>
   </div>
 </template>
 
@@ -9,7 +9,23 @@ export default {
   name: 'videoBG',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      vimeoVideos: []
+    }
+  },
+  mounted: function () {
+    this.$http.get(
+      'https://api.vimeo.com/me/videos',
+      {headers: {'Authorization': 'bearer 1bb5838a1c16bdab1e8eac3add1b6f2a'}})
+    .then(response => {
+      response.body.data.filter(this.filterVideos)
+    }, response => {
+    })
+  },
+  methods: {
+    filterVideos: function (video) {
+      if (this.vimeoVideos.length <= 5 && video.name.includes('##')) {
+        this.vimeoVideos.push(video)
+      }
     }
   }
 }
